1312840ce2fbe8b60ce431d5a6e3548f45af4530
init1-dev <init1.dev@gmail.com>
2024-07-08 16:14:53 +0200

    FIX: hide zoom component in mobile devices

--- src/components/Image/ZoomComponent.tsx
+++ src/components/Image/ZoomComponent.tsx
@@ -50,7 +50,7 @@ function ZoomComponent({
 const ZoomContainer = styled.div`
     display: none;
 
-    @media(min-width: 1500px)  {
+    @media(min-width: 1000px)  {
         position: absolute;
         display: flex;
         flex-direction: column;
